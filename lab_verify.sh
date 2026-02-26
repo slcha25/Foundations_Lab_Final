2e2df16dbfaf0f448758b9f3cc2283d7f7e64532
slcha25 <s.leng.chan@outlook.com>
2026-02-26 20:36:31 +0000

    Night 3 Infrastructure Audit Complete Update version

--- lab_verify.sh
+++ lab_verify.sh
@@ -3,39 +3,40 @@
 # Foundations Lab Final - Infrastruture Audit Script
 # Target: Ubuntu 24.04 LTS
 
- OUTPUT_FILE = "setup_verify.txt"
 
-echo "Starting Infrastructure Audit..."> $OUTPUT_FILE
-echo "Timestamp:$(date)" >> $OUTPUT_FILE
-echo "_________________________________" >> $OUTPUT_FILE
+
+echo "Starting Infrastructure Audit..."> setup_verify.txt
+echo "Timestamp:$(date)" >> setup_verify.txt
+echo "_________________________________" >> setup_verify.txt
 
 # 1. Check Git Installation
  if command -v git &> /dev/null; then
-	echo "Git: INSTALLED" >> $OUTPUT_FILE
+	echo "Git: INSTALLED" >> setup_verify.txt
  else 
-	echo "Git: NOT FOUND" >> $OUTPUT_FILE
+	echo "Git: NOT FOUND" >> setup_verify.txt
  fi
 
 # 2. Check Python 3 Installation
  if command -v python3 &> /dev/null; then
-	echo "Python3: INSTALLED" >> $OUTPUT_FILE
+	echo "Python3: INSTALLED" >> setup_verify.txt
  else
-	echo "Python3: NOT FOUND" >> $OUTPUT_FILE
+	echo "Python3: NOT FOUND" >> setup_verify.txt
  fi
 
 # 3. Check Network IP
 # This is command pulls the internal IP address associated with the hostname 
 IP_ADDR=$(hostname -I | awk '{print $1}')
 
- if [ -z "$IP_ADDR"]; then
-	echo "Network IP: OFFLINE" >> $OUTPUT_FILE
+ if [ -z "$IP_ADDR" ]; then
+	echo "Network IP: OFFLINE" >> setup_verify.txt
  else
-	echo "Network IP: $IP_ADDR" >> $OUTPUT_FILE
+	echo "Network IP: $IP_ADDR" >> setup_verify.txt
  fi
 
-echo "_________________________________" >> $OUTPUT_FILE
+echo "_________________________________" >> setup_verify.txt
 echo "Audit Complete. Results saved to $OUTPUT_FILE"
 
-cat $OUTPUT_FILE 
+cat setup_verify.txt
+
 
 
